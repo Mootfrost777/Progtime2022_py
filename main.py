743fe0126758ab8dced331097f1238a080b45a8f
Andrey <andrey13102007@gmail.com>
2022-02-28 20:28:55 +0300

    Simple registration added.

--- main.py
+++ main.py
@@ -1,56 +1,80 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import PlainTextResponse
+from enum import Enum, auto
+
 import uvicorn
+from fastapi import FastAPI, Body
 import sqlite3
 
 app = FastAPI()
+
+
+class DBAction(Enum):
+    fetchone = auto()
+    fetchall = auto()
+    commit = auto()
+
+
+def db_action(sql: str, args: tuple, action: DBAction):
+    conn = sqlite3.connect('db.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute(sql, args)
+    if action == DBAction.fetchone:
+        result = cursor.fetchone()
+    elif action == DBAction.fetchall:
+        result = cursor.fetchall()
+    elif action == DBAction.commit:
+        conn.commit()
+        result = None
+
+    cursor.close()
+    conn.close()
+
+    return result
 
 
 @app.on_event('startup')
 def create_db():
     conn = sqlite3.connect('db.sqlite')
     cursor = conn.cursor()
+
     cursor.execute('''
         CREATE TABLE if NOT EXISTS users (
             id INTEGER PRIMARY KEY,
             username VARCHAR NOT NULL,
             password VARCHAR NOT NULL
-        )
+        );
     ''')
+
     cursor.close()
     conn.close()
 
 
 @app.get('/')
 def index():
-    return PlainTextResponse('Any text :)')
+    return 'Hello, world!'
+
+
+@app.post('/signup')
+def signup(username: str = Body(...), password: str = Body(...)):
+    return db_action(
+        '''
+            INSERT INTO users (username, password) VALUES (username, password)
+        ''',
+        (),
+        DBAction.commit,
+    )
 
 
 @app.post('/login')
 def login(username: str = Body(...), password: str = Body(...)):
-    conn = sqlite3.connect('db.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('''
-        SELECT * FROM users WHERE username = ? AND password = ?
-    ''', (username, password))
-    user = cursor.fetchone()
-    cursor.close()
-    conn.close()
-    return user
-
-
-@app.post('/test')
-def test():
-    conn = sqlite3.connect('db.sqlite')
-    cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO users (username, password) VALUES ('1', '1')
-    ''')
-    user = cursor.fetchone()
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return user
+    return db_action(
+        '''
+            SELECT * FROM users WHERE username = ? AND password = ?
+        ''',
+        (username, password),
+        DBAction.fetchone,
+    )
 
 
 uvicorn.run(app)
+
